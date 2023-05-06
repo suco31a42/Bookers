ba307a8a4d0f4bd29c191c9ecde6c1e5920a48e7
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-06 03:23:56 +0000

    [add]3-13トップ画面作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'homes#top'
+  get 'homes/top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
