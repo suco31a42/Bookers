6690ce4807818df6435c069bcdbc4fc9098a3419
AyakaTakahashi <132245813+suco31a42@users.noreply.github.com>
2023-05-07 09:57:19 +0000

    [add]3-13 アプリ完成(仮

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,47 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(params)
-    book.save
-    redirect_to book_path(@book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
-  
-  def new
-   
-  end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'homes#top'
+  root to: 'homes#top'
 
   resources :books
   
